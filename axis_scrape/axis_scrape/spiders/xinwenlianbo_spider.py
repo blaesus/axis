acc07eae1775a7eca2d69b2cc0896f6657044521
andyshuxin <andy@shux.in>
2016-02-12 12:07:17 +0800

    fix: use correct logic for url generation

--- axis_scrape/axis_scrape/spiders/xinwenlianbo_spider.py
+++ axis_scrape/axis_scrape/spiders/xinwenlianbo_spider.py
@@ -1,76 +1,92 @@
 import scrapy
 from datetime import date, timedelta
 
-def getIndexUrls(start_date=None, end_date=None):
+# Naming indices for use in combination with url_schemes
+DATE = 0
+URL_FUNCTION = 1
+
+
+def get_url_schemes():
+    """
+    See official JavaScript implementation, such as source code of
+    http://news.cntv.cn/program/xwlb/20100506.shtml:
+
+    if(time_1<20100506){
+    str = "http://news.cctv.com/program/xwlb/" + year + mon + day + ".shtml";
+    }
+    if(time_1>=20100506&&time_1<20110406){
+    str = "http://news.cntv.cn/program/xwlb/" + year + mon + day + ".shtml";
+    }
+    if(time_1>=20110406){
+    str = "http://cctv.cntv.cn/lm/xinwenlianbo/" + year + mon + day + ".shtml";
+    }
+    """
+
+    # [(Start-date, URL function)]
+    return (
+        (date(2002, 9, 8), lambda s:
+            'http://www.cctv.com/news/xwlb/' + s + '/index.shtml'),
+        (date(2009, 6, 27), lambda s:
+            'http://news.cctv.com/program/xwlb/' + s + '.shtml'),
+        (date(2010, 5, 6), lambda s:
+            'http://news.cntv.cn/program/xwlb/' + s + '.shtml'),
+        (date(2011, 4, 6), lambda s:
+            'http://cctv.cntv.cn/lm/xinwenlianbo/' + s + '.shtml'),
+    )
+
+
+def get_index_urls(start_date=None, end_date=None):
     """
     :param start_date:
     :param end_date:
     :return: {dict} such as {'http://stuff.com/...': date(2015, 3, 4)}
-
-    Official JavaScript implementation, see source code of
-    http://news.cntv.cn/program/xwlb/20100506.shtml:
-
-    if(time_1<20100506){
-        str = "http://news.cctv.com/program/xwlb/" + year + mon + day + ".shtml";
-    }
-    if(time_1>=20100506&&time_1<20110406){
-        str = "http://news.cntv.cn/program/xwlb/" + year + mon + day + ".shtml";
-    }
-    if(time_1>=20110406){
-        str = "http://cctv.cntv.cn/lm/xinwenlianbo/" + year + mon + day + ".shtml";
-    }
     """
 
-    FIRST_AVAILABLE_DATE = date(2009, 6, 27)
-    URL_SCHEMA_CHANGE_DATE_A = date(2010, 5, 6)
-    URL_SCHEMA_CHANGE_DATE_B = date(2011, 4, 6)
+    url_schemes = get_url_schemes()
 
     if not start_date:
-        start_date = FIRST_AVAILABLE_DATE
+        start_date = url_schemes[0][DATE]
     if not end_date:
         end_date = date.today()
-
     index_urls = {}
+
     today = start_date
     while today < end_date:
         date_numerals = str(today).replace('-', '')
-        if today < URL_SCHEMA_CHANGE_DATE_A:
-            index_url = 'http://news.cctv.com/program/xwlb/' + \
-                        date_numerals + '.shtml'
-        elif today < URL_SCHEMA_CHANGE_DATE_B:
-            index_url = 'http://news.cntv.cn/program/xwlb/' + \
-                        date_numerals + '.shtml'
-        else:
-            index_url = 'http://cctv.cntv.cn/lm/xinwenlianbo/' + \
-                        date_numerals + '.shtml'
+
+        index_url = None
+        for scheme in reversed(url_schemes):
+            if scheme[DATE] <= today:
+                index_url = scheme[URL_FUNCTION](date_numerals)
+                break
+
         index_urls[index_url] = today
         today += timedelta(days=1)
-    print('index_urls', index_urls)
     return index_urls
 
 
 def extract_article_links_a(response):
-    pass
+    return []
 
 
 def extract_article_links_b1(response):
-    pass
+    return []
 
 
 def extract_article_links_b2(response):
-    pass
+    return []
 
 
 def extract_article_links_b3(response):
-    pass
+    return []
 
 
 def extract_article_links_b4(response):
-    pass
+    return []
 
 
 def extract_article_links_latest(response):
-    pass
+    return []
 
 
 def get_period_definitions():
@@ -114,9 +130,9 @@ def get_period_definitions():
     ]
     return periods
 
-indexUrls = getIndexUrls()
+# indexUrls = get_index_urls()
 # Debug
-indexUrls = getIndexUrls(date(2016, 2, 5), date.today())
+indexUrls = get_index_urls(date(2010, 5, 1), date(2010, 5, 10))
 
 
 def getPeriod(target_date, periods):
